87a10b9c1120ffb6a7609ef140744955f7302ae4
oceansalle <oceanesalleiae@gmail.com>
2023-03-15 11:00:48 +0100

    standardMeassage

--- client/src/components/chat/index.jsx
+++ client/src/components/chat/index.jsx
@@ -5,6 +5,7 @@ import {
   MultiChatWindow,
 } from "react-chat-engine-advanced";
 import Header from "@/components/customHeader";
+import StandardMessageForm from "@/components/customMessageForms/StandardMessageForm";
 
 
 const Chat = () => {
@@ -14,14 +15,31 @@ const Chat = () => {
     "1234"
   )
 
-  return <div style={{ flexBasis: "100%" }}>
+  return (
+    <div style={{ flexBasis: "100%" }}>
       <MultiChatSocket {...chatProps} />
       <MultiChatWindow
         {...chatProps}
         style={{ height: "100vh" }}
         renderChatHeader={(chat) => <Header chat={chat} />}
+        renderMessageForm={(props) => {
+          if (chatProps.chat?.title.startsWith("AiChat_")) {
+            return <Ai props={props} activeChat={chatProps.chat} />;
+          }
+          if (chatProps.chat?.title.startsWith("AiCode_")) {
+            return <AiCode props={props} activeChat={chatProps.chat} />;
+          }
+          if (chatProps.chat?.title.startsWith("AiAssist_")) {
+            return <AiAssist props={props} activeChat={chatProps.chat} />;
+          }
+
+          return (
+            <StandardMessageForm props={props} activeChat={chatProps.chat} />
+          );
+        }}
       />
-      </div>; 
+    </div>
+  );
 };
 
-export default chat
+export default Chat;
